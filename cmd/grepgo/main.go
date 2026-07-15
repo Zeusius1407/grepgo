0df995f1691e4e39604148a9298799dccf4ccf07
Tejas Agarwal <tejasagarwal@Tejass-Laptop-2.local>
2026-07-15 20:32:12 +0530

    Restructure

--- cmd/grepgo/main.go
+++ cmd/grepgo/main.go
@@ -4,7 +4,7 @@ Copyright © 2026 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/Zeusius1407/mygrep/cmd"
+import "github.com/Zeusius1407/mygrep/internal"
 
 func main() {
 	cmd.Execute()
